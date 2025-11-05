9e36a4557cc705c4810bf706916455dd0c95c766
d0s1 <dominic1020@web.de>
2025-11-05 17:06:04 +0100

    you can now answers with the answers from the api, might need some improvement

--- js/app.js
+++ js/app.js
@@ -35,4 +35,12 @@ export function decodeHTML(str) {
   return txt.value;
 }
 
+export function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
 
